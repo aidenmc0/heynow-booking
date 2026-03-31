4efb78a7ff1f0537fbeeeaaa8d85eb53086b0750
aidenmc0 <aidenmccourtt@gmail.com>
2026-03-31 09:50:47 +0000

    ok

--- src/App.jsx
+++ src/App.jsx
@@ -14,6 +14,7 @@ import RoomDetail from "./components/RoomDetail";
 import Layout from "./components/Layout";
 import ShowCalendar from "./components/ShowCalendar";
 import Restaurant from "./components/Restaurant";
+import BookingRoom from "./components/Booking"
 
 function AppContent() {
   const { lang } = useLanguage(); // ดึงภาษาปัจจุบัน
@@ -40,7 +41,7 @@ function AppContent() {
           }
         />
         <Route path="/rooms/:id" element={<RoomDetail />} />
-        <Route path="/booking" element={<BookingRoom />} />
+        <Route path="/booking" element={<BookingRoom/>} />
       </Routes>
     </main>
   );
